e24e650dd99345c37ca673e2de0ed7f925aded74
Dmytro Morozov <dmm@timemsystem.com>
2015-04-02 10:23:03 +0200

    Initial TLS support.

--- server.js
+++ server.js
@@ -1,59 +1,23 @@
 #!/usr/bin/env node
-
-/**
- * Module dependencies.
- */
-
 'use strict';
 
-var app = require('./app');
-var debug = require('debug')('expenses:server');
-var http = require('http');
-
-/**
- * Get port from environment and store in Express.
- */
-
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-
-var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-/**
- * Normalize a port into a number, string, or false.
- */
+var app = require('./app'),
+    debug = require('debug')('expenses:server'),
+    server;
 
 function normalizePort(val) {
   var port = parseInt(val, 10);
 
   if (isNaN(port)) {
-    // named pipe
     return val;
   }
 
   if (port >= 0) {
-    // port number
     return port;
   }
 
   return false;
 }
-
-/**
- * Event listener for HTTP server "error" event.
- */
 
 function onError(error) {
   if (error.syscall !== 'listen') {
@@ -77,12 +41,49 @@ function onError(error) {
   }
 }
 
-/**
- * Event listener for HTTP server "listening" event.
- */
-
 function onListening() {
   var addr = server.address();
   var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
   debug('Listening on ' + bind);
+}
+
+function done() {
+  server.listen(port);
+  server.on('error', onError);
+  server.on('listening', onListening);
+}
+
+function tls() {
+  var https = require('https'),
+      azureStorage = require('azure-storage'),
+      blobService = azureStorage.createBlobService(),
+      container = 'pem';
+  blobService.getBlobToText(container, 'key.pem', function (keyError, key) {
+    if (keyError) {
+      throw keyError;
+    }
+    blobService.getBlobToText(container, 'cert.pem', function (certError, cert) {
+      if (certError) {
+        throw certError;
+      }
+      var options = {
+        key: key,
+        cert: cert
+      };
+      server = https.createServer(options, app);
+      done();
+    });
+  });
+}
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+if (app.get('env') === 'development') {
+  var http = require('http');
+  server = http.createServer(app);
+  done();
+}
+else {
+  tls();
 }
