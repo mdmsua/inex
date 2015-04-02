a1c951e785313a83e6455d5018f07b67f42f6b53
Dmytro Morozov <dmm@timemsystem.com>
2015-04-02 13:21:44 +0200

    Updated .jshintrc and Gruntfile with angular/node targets

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,19 +16,36 @@ module.exports = function (grunt) {
                 files: [
                     {
                         expand: true, flatten: true, dest: 'public/css', filter: 'isFile',
-                        src: ["bower_components/**/*.min.css", "!bower_components/angular-material/modules/**"]
+                        src: ['bower_components/**/*.min.css', '!bower_components/angular-material/modules/**']
                     },
                     {
                         expand: true, flatten: true, dest: 'public/js', filter: 'isFile',
-                        src: ["bower_components/**/*.min.js", "!bower_components/angular-material/modules/**"]
+                        src: ['bower_components/**/*.min.js', '!bower_components/angular-material/modules/**']
                     }
                 ]
             }
         },
         jshint: {
-            all: ['app/**/*.js'],
             options: {
                 jshintrc: '.jshintrc'
+            },
+            angular: {
+                options: {
+                    node: false
+                },
+                files: {
+                    src: ['app/**/*js']
+                }
+            },
+            node: {
+                options: {
+                    globals: {
+                        angular: false
+                    }
+                },
+                files: {
+                    src: ['routes/*.js', 'app.js', 'server.js']
+                }
             }
         }
     });
